7c93bd810f8d2a26a11b0d729aa2dd98a3afa482
Anastasia Grigoryeva <87610885+staciess@users.noreply.github.com>
2025-05-12 15:48:53 +0300

    Support plant search

--- src/main/kotlin/org/redisco/worldofplants/repository/PlantsRepository.kt
+++ src/main/kotlin/org/redisco/worldofplants/repository/PlantsRepository.kt
@@ -2,9 +2,14 @@ package org.redisco.worldofplants.repository
 
 import org.redisco.worldofplants.entities.PlantEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.util.UUID
 
 interface PlantsRepository : JpaRepository<PlantEntity, UUID> {
     fun findPlantEntitiesByCategory(category: String): List<PlantEntity>
     fun findPlantEntityByName(name: String): PlantEntity?
+
+    @Query("SELECT p FROM PlantEntity p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR LOWER(p.description) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
+    fun searchPlants(@Param("searchTerm") searchTerm: String): List<PlantEntity>
 }
